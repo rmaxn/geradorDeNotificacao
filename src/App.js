f12c045fd38f4203a32bc96d6e9fb2ced478a7eb
Robertson <robertson.nunes@gmail.com>
2022-05-19 22:16:32 -0300

    cria formulario

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
 import './App.css';
+
+function gerarNotificacao() {
+  let bloco = document.querySelector('#bloco').value
+  let apto = document.querySelector('#apto').value
+  let motivo = document.querySelector('#motivo').value
+  let tipo = document.querySelector('#tipo').value
+  let data = document.querySelector('#date').value
+    .split('-').reverse().join('/')
+  let hour = (document.querySelector('#hour').value
+    .split(':').join('h') + 'm')
+  return console.log(bloco, apto, motivo, tipo, data, hour)
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="formulario">
+      <form>
+        <label htmlFor="bloco">Bloco:
+        <input name="bloco" id="bloco" placeholder="Bloco"></input>
+        </label>
+        <label htmlFor="apto">Apto:
+        <input id="apto" placeholder="Apartamento"></input>
+        </label>
+        <label htmlFor="motivo">Motivo:
+        <select id="motivo">
+          <option>Objeto na Janela</option>
+          <option>Barulho</option>
+        </select>
+        </label>
+        <label htmlFor="tipo">Tipo:
+        <select id="tipo">
+          <option>Advertência</option>
+          <option>Multa</option>
+        </select>
+        </label>
+        <label htmlFor="data">Data:
+        <input type="date" id="date"/>
+        </label>
+        <label htmlFor="hour">Hora:
+        <input type="time" id="hour"/>
+        </label>
+      </form>
+      <button id="gerar" onClick= { () => gerarNotificacao() }>Gerar Notificação</button>
     </div>
   );
 }
